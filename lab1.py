be17a292ed6eff7f193ade2172a53585d9d9d2c7
nuclearsh123 <145000362+nuclearsh123@users.noreply.github.com>
2023-09-21 14:18:36 +0530

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,54 @@
+# x=int(input("enter number"))
+# y=int(input("enter number"))
+# addition=x+y
+# substraction=x-y
+# multiplication=x*y
+# division=x/y
+# print(addition,substraction,multiplication,division)
+
+
+
+
+
+
+# x=int(input("enter number"))
+# y=int(input("enter number"))
+# z=int(input("enter number"))
+# t=int(input("enter number"))
+# avg=x+y+z+t/4
+# print("avg")
+
+
+
+# p=int(input("principal"))
+# r=int(input('rate of interest'))
+# t=int(input("time"))
+# simple_interest=(p*r*t)/100
+# print(simple_interest)
+
+
+# s=int(input("distance in KM"))
+# t=int(input("time in minutes"))
+# speed=s/t
+# print(speed) 
+
+# x=int(input("enter number"))
+# y=int(input("enter number"))
+# z=int(input("enter number"))
+# s=(x+y+z)/2
+# area=(s*(s-x)*(s-y)*(s-z))**0.5
+# print(area)
+
+
+
+# x=int(input("enter number"))
+# y=int(input("enter number"))
+# z=int(input("enter number"))
+# #Eqn=a*x*x*+b*x+c
+# D=y*y-4*x*z
+# p_1=(-y+D**(0.5))/2*x
+# p_2=(-y-D**(0.5))/2*x
+# if(D>=0):
+#     print(p_1,p_2)
+# elif(D<0):
+#     print('imaginary roots')
